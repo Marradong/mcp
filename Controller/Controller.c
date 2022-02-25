7fa89a8d5a6f1a3b3ae1e01187c6c278d540c502
paxmaniac <jonpaxman@gmail.com>
2022-02-25 15:26:50 +0800

    Update Controller.c

--- Controller/Controller.c
+++ Controller/Controller.c
@@ -1,7 +1,5 @@
 //Example ATmega2560 Project
 //File: ATmega2560Project.c
-//Author: Robert Howie
-//Created: March 2015
 //An example file for second year mechatronics project
 
 //include this .c file's header file
@@ -11,18 +9,13 @@
 
 
 //file scope variables
-static char serial_string[200] = {0}; //declare and initialise string for serial printing
-static char lcd_string[33] = {0}; //declare and initialise string for LCD
 
 int main(void)
 {
 	//variable declarations
 	
 	//initialisation section, runs once
-	serial0_init(); //initialise serial subsystem
-	adc_init(); //initialse ADC
-	lcd_init(); //initialise 
-	milliseconds_init(); //initialise timer3 to track milliseconds
+	
 	_delay_ms(20);
 	
 	//main loop
